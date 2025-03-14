fa310a401699b2325036d72d0e224808ad0aa38e
devopsjune2024 <devops.trainer12@gmail.com>
2025-03-14 07:24:02 +0530

    FirstJavapp

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hey-- Devops!" );
+        System.out.println( "Hey-- March, This is Sample MavenTest" );
     }
 }
